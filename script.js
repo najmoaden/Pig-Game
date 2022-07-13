76decbac3d712993639e46129ca8880d1d2b9f44
najmoaden <78856004+najmoaden@users.noreply.github.com>
2022-07-13 14:12:52 +0300

    Switching the active player.

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 //Selecting elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
@@ -16,7 +18,9 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 //Roll dice functionality
 btnRoll.addEventListener('click', function () {
@@ -30,9 +34,19 @@ btnRoll.addEventListener('click', function () {
   if (dice !== 1) {
     //Add score to the current score
     currentScore += dice;
-    current0El.textContent = currentScore; //CHANGE LATER
-  }
-  //switch to next player
-  else {
+    //Add to the active player's current score
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+
+    /* //Display the score of player 0 only (This one is deleted)
+    current0El.textContent = currentScore; //CHANGE LATER */
+  } else {
+    //Set current player's score to 0
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    //switch to next player
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   }
 });
