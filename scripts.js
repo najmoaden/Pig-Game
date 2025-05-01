099e21dd725e430fc4cc223fa2198a8a04986623
Najmo Aden <najmoaden138@gmail.com>
2025-05-01 14:50:38 +0300

    :recycle: feat: reset the game

--- scripts.js
+++ scripts.js
@@ -12,15 +12,29 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
+let scores, currentScore, activePlayer, playing;
 //Starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+//Function to initialize the game
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+  //Visible on the screen
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+  diceEl.classList.add('hidden');
+};
+
+init(); // Call the function to initialize the game
 
 //Function to switch players
 const switchPlayer = function () {
@@ -60,7 +74,7 @@ btnHold.addEventListener('click', function () {
     scores[activePlayer] += currentScore;
     document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
     //2. Check if player's score is >= 100
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= 100) {
       //Finish game
       playing = false;
       diceEl.classList.add('hidden');
@@ -72,3 +86,6 @@ btnHold.addEventListener('click', function () {
     }
   }
 });
+
+//Resetting the game
+btnNew.addEventListener('click', init);
